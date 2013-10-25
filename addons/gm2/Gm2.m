091581cfc1ffb5b6458d0aced1f9ee4ab974289d
Robert Greifenhagen <Robert.Greifenhagen@physik.tu-dresden.de>
2013-10-25 17:40:38 +0200

    cut actuell useless expressions

--- addons/gm2/Gm2.m
+++ addons/gm2/Gm2.m
@@ -7,27 +7,16 @@ F1C;
 F2C;
 F1N;
 F2N;
-g2;
-g1;
-ymu;
-VCha;
-UCha;
-ZNeu;
-USf;
 MM;
-MW;
-CB;
-SW;
-CW;
 EL;
 ELGmu;
 MCha;
 MNeu;
 MSf;
-i;
-k;
-m;
-
+AAN;
+AAC;
+BBN;
+BBC;
 
 
 Print["*****************************************************"];
@@ -39,33 +28,7 @@ Print[""];
 Begin["`Private`"];
 
 CalculateGm2OneLoop[] :=
-    Module[{result,cL,cR,nL,nR,ymu,g1,g2,AAC,AAN,BBC,BBN,xpm,x0},
-
-           cL = - g2 VCha[k,1];
-           cR = ymu  UCha[k,2];
-           nL = 1 / Sqrt[2] ( g1 ZNeu[i,1] + g2 ZNeu[i,2] ) *
-           Conjugate[ USf[2,2][m,1] ] - ymu ZNeu[i,3]  Conjugate[ USf[2,2][m,2] ];
-           nR = Sqrt[2] g1 ZNeu[i,1]  USf[2,2][m,2]  + ymu ZNeu[i,3]  USf[2,2][m,1];
-
-           ymu = MM g2 / (Sqrt[2] MW CB);
-           g1  = g2 * SW/CW;
-           g2  = EL / SW;
-
-(* MyConjugate only works for exactly the expressions which appear here *)
-
-           MyConjugate[expr_] :=expr/.{USf[ii__][jj__]->Conjugate[USf[ii][jj]],
-                            ZNeu[ii__]     ->Conjugate[ZNeu[ii]],
-                            VCha[ii__]     ->Conjugate[VCha[ii]],
-                            UCha[ii__]     ->Conjugate[UCha[ii]]};
-
-           AAC = MyConjugate[cL] cL + MyConjugate[cR] cR;
-           AAN = MyConjugate[nL] nL + MyConjugate[nR] nR;
-           BBC = (cL cR + MyConjugate[cL cR]) / ( 2 MM );
-           BBN = (nL nR + MyConjugate[nL nR]) / ( 2 MM );
-
-           xpm = MCha[k]^2 / MSf[1,1,2]^2;
-           x0  = MNeu[i]^2 / MSf[m,2,2]^2;
-
+    Module[{result,i,m,k,x0,xpm},
 
            amuChipm[k_] = ELGmu^2 / EL^2 * 1 / (16Pi^2) MM^2 / MSf[1,1,2]^2
                           * (1/12 AAC F1C[xpm]  +  2 MCha[k] / 3 BBC F2C[xpm]);
